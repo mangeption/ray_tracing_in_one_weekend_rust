b279bf192a05f01ba5e0b40d1e17c92fa1a9866a
kyle nguyen <ngockhanh5794@gmail.com>
2021-07-04 13:30:49 +0800

    Adding sphere

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,20 @@ fn main() {
 }
 
 fn ray_color(r: Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_dir = r.direction.unit_vector();
     let t = 0.5 * (unit_dir.y() + 1.0);
 
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+    let oc = r.origin - center;
+    let a = Vec3::dot(r.direction, r.direction);
+    let b = 2.0 * Vec3::dot(oc, r.direction);
+    let c = Vec3::dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
 }
