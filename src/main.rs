7a2f366ba28d45e7f86707305803997bd7af0a81
kyle nguyen <ngockhanh5794@gmail.com>
2021-07-04 12:57:44 +0800

    Sending rays into the scene

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,46 @@ mod ray;
 mod vec3;
 
 use ray::Ray;
-use vec3::Color;
+use vec3::{Color, Point3, Vec3};
 
 fn main() {
-    const IMAGE_WIDTH: i64 = 256;
-    const IMAGE_HEIGHT: i64 = 256;
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i64 = 400;
+    const IMAGE_HEIGHT: i64 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i64;
+
+    // Camera
+    let viewport_height: f64 = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length: f64 = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    // Render
 
     print!("P3\n{:?} {:?}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = (IMAGE_HEIGHT - j - 1) as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b: f64 = 0.25;
-
-            let color = Color::new(r, g, b);
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = (IMAGE_HEIGHT - j - 1) as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let color = ray_color(r);
             color.write_color();
         }
     }
+}
+
+fn ray_color(r: Ray) -> Color {
+    let unit_dir = r.direction.unit_vector();
+    let t = 0.5 * (unit_dir.y() + 1.0);
+
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
